1788e98693b4921b1c2e372eb8f5eae7b5d9cfab
lkkikkil <skattr13@naver.com>
2023-01-12 18:05:39 +0900

    Style: 해시, 프로필, 인풋 수정
    여러가지 수정했습니다~
    테스트컴포넌트, 헤더, 네브 삭제해주세요!

---
+++ src/component/header/HeaderComponent.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Img from "../basic/Img"
+import { Input } from "../basic/Input"
+import { PointerDiv, BorderDiv, FlexDiv, Div, PositionDiv } from "../basic/Div"
+import { MdButton } from "../basic/Button"
+import { IconText } from "../common/IconText"
+
+const HeaderComponent = () => {
+
+    return (
+        <FlexDiv>
+            <PointerDiv width="" height="100%"><Img /></PointerDiv>
+        </FlexDiv>
+    )
+}
